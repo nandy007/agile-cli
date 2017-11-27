6cacd08b1dd47fa54c6e0a2afedbca7c50a0d390
nandy007 <nandy_wong@126.com>
2017-11-27 15:21:20 +0800

    完善模板内容

--- source/template-auicomponent-amd/src/public/assets/js/main.js
+++ source/template-auicomponent-amd/src/public/assets/js/main.js
@@ -1,12 +1,13 @@
 require.config({
 	waitSeconds: 0,
 	baseUrl: './assets',
-	urlArgs: "r=" + (new Date()).getTime(),
+	//urlArgs: "r=" + (new Date()).getTime(),
     paths: {
 		'css': './frameworks/amd/css',
 		'less': './frameworks/amd/less',
 		'lessc': './frameworks/amd/lessc',
 		'normalize': './frameworks/amd/normalize',
+		'sass': './frameworks/amd/sass.sync',
 		'aui': './frameworks/agile/aui',
     	'agile-ce': './frameworks/agile/agile.ce.browser',// 必须
     	'agile-ui': './frameworks/agile/agile.ui'// 必须
